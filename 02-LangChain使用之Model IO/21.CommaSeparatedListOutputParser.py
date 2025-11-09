58825f7aff1cb080c1c5c62e89788c1e88dc399f
“杨鑫昊” <yangxinhao.9527@bytedance.com>
2025-11-09 14:57:27 +0800

    feat(Model IO): 添加CommaSeparatedListOutputParser示例并修复JsonOutputParser格式

--- 02-LangChain使用之Model IO/21.CommaSeparatedListOutputParser.py
+++ 02-LangChain使用之Model IO/21.CommaSeparatedListOutputParser.py
@@ -4,8 +4,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage
 from pydantic import SecretStr
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
 dotenv.load_dotenv()
 
@@ -17,29 +17,37 @@ api_key = SecretStr(api_key_str) if api_key_str else None
 
 chat_model = ChatOpenAI(model=model_name, base_url=base_url, api_key=api_key)
 
-joke_query = "告诉我一个笑话。"
-# 定义Json解析器
-parser = JsonOutputParser()
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [("system", "你是一个靠谱的{role}"), ("human", "{question}")]
+)
+
+
+output_parser = CommaSeparatedListOutputParser()
 
 # 返回一些指令或模板，这些指令告诉系统如何解析或格式化输出数据
-format_instructions = parser.get_format_instructions()
+format_instructions = output_parser.get_format_instructions()
 print("format_instructions:", format_instructions)
 print("\n")
 
-# 定义提示词模版
-# 注意，提示词模板中需要部分格式化解析器的格式要求format_instructions
+messages = "大象,猩猩,狮子"
+result = output_parser.parse(messages)
+print("result:", result)
+print(type(result))
+print("\n")
+
+
 prompt = PromptTemplate(
-    template="回答用户的查询.\n{format_instructions}\n{query}\n",
-    input_variables=["query"],
+    template="生成5个关于{text}的列表，使用中文回复 .\n\n{format_instructions}",
+    input_variables=["text"],
     # 预填充某些变量的值，避免重复传入
     partial_variables={"format_instructions": format_instructions},
 )
 
-print(prompt.format(query=joke_query))
+print(prompt.format(text="动物"))
 print("\n")
 
 # 5.使用LCEL语法组合一个简单的链
-chain = prompt | chat_model | parser
+chain = prompt | chat_model | output_parser
 # 6.执行链
-output = chain.invoke({"query": "给我讲一个笑话"})
+output = chain.invoke({"text": "动物"})
 print(output)
